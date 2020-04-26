49584794e7f41905010b370c51b583181e92ed4c
afif.alfiano <afif.alfiano@uii.ac.id>
2020-04-26 21:43:49 +0700

    fix frontend and backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,4 +12,7 @@ Route::group([
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 
+    Route::post('sendPasswordResetLink', 'ResetPasswordController@sendEmail');
+    Route::post('resetPassword', 'ChangePasswordController@process');
+
 });
