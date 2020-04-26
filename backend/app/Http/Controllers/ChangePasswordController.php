65183906c4985876b26f20791817de63ca43bac3
afif.alfiano <afif.alfiano@uii.ac.id>
2020-04-26 21:47:05 +0700

    fix folder fe

---
+++ backend/app/Http/Controllers/ChangePasswordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ChangePasswordRequest;
+use DB;
+use App\User;
+
+class ChangePasswordController extends Controller
+{
+    public function process(ChangePasswordRequest $request) {
+        return $this->getPasswordResetTableRow($request)->count() > 0 ? $this->changePassword($request): 
+        $this->tokenNotFoundResponse();
+    }
+
+    private function getPasswordResetTableRow($request) {
+        return DB::table('password_resets')->where(['email'=>$request->email, 'token' => $request->resetToken]);
+    }
+
+    private function tokenNotFoundResponse() {
+
+        return \response()->json([
+            'email' => 'Token or Email is incorrect'
+        ], 400);
+    }
+
+    private function changePassword($request) {
+
+        $user = User::whereEmail($request->email)->first();
+        $user->update(['password' => $request->password]);
+        $this->getPasswordResetTableRow($request)->delete();
+        return \response()->json([
+            'data' => 'Password Successfully Changed'
+        ], 200);
+    }
+}
